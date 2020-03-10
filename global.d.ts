51e0df3da9b14037ace1ee9cd14c872f148f033c
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2020-03-10 00:13:07 -0500

    [ FIX ] global

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,6 @@
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
-declare module "react-native-gesture-handler" {
+declare module 'react-native-gesture-handler' {
   /* eslint-disable no-undef */
-  export class TouchableNativeFeedback extends React.Component<any, any > {}
+  export class TouchableNativeFeedback extends React.Component<any, any> {}
 }
